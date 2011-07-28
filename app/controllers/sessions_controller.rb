8072c0c35eb8c57e40e305ddfe1f6ccdcc2bfd1a
sbhalerao <sbhalerao@gmail.com>
2011-07-27 23:36:33 -0700

    debugging callback

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,7 @@
 class SessionsController < ApplicationController
   
   def create
-  auth=request.env["omniauth.auth"]
-  user=User.find_by_provider_and_uid(auth["provider"],auth["uid"]) || User.create_with_omniauth(auth)
-  session[:user_id] = user.id  
-  redirect_to root_url, :notice => "Signed in!"
+  raise request.env["omniauth.auth"].to_yaml
+  
   end
 end

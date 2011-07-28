9f0b5210332f7cb9bdb819d27f9842ce2f4d7d73
sbhalerao <sbhalerao@gmail.com>
2011-07-28 11:12:07 -0700

    oauth for fb

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :twitter, 'AnSYCY4bRcmfUBJODfpmQ', 'GyvfClXeCLXL4NJBKGU7glpXDMfVDBN37pjjeQzFzw'
+  provider :facebook, '250050248346256','9735ae616f47b11f109748800d05f877'
 end

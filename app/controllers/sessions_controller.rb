2763f2854e1afa1156db1b8ba9f225196e33b500
sbhalerao <sbhalerao@gmail.com>
2011-07-27 23:52:04 -0700

    oauth debug

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,7 +1,8 @@
-class SessionsController < ApplicationController
-  
-  def create
-  raise request.env["omniauth.auth"].to_yaml
-  
-  end
+class SessionsController < ApplicationController  
+  def create  
+    auth = request.env["omniauth.auth"]  
+    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)  
+    session[:user_id] = user.id  
+    redirect_to root_url, :notice => "Signed in!"  
+  end  
 end

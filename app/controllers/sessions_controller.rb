2b7c85e499a8e555ed587d49d9457bdbaaa2724d
sbhalerao <sbhalerao@gmail.com>
2011-07-28 01:04:08 -0700

    aouth:

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
         user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)  
       end
       
-      failure
+      def failure
         flash[:error] = 'There was an error at the remote authentication service. You have not been signed in.'
         redirect_to root_url
       end
